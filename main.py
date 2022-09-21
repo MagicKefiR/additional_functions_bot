d272db06bab54486cf65270729d9a851f453926c
MagicKefiR <3939723@gmail.com>
2022-09-22 01:07:25 +0300

    сбор id

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from decouple import config
 bot = telebot.TeleBot(config('TELEGRAM_TOKEN'))
 
 
-@bot.message_handler(commands=['all'])
-def start_message(message):
-    bot.send_message(message.chat.id, '@FpsPerSecond')
+@bot.message_handler(commands=['start'])
+def greet(message):
+  print(str(message.from_user.first_name) + " - " + str(message.from_user.id))
+  bot.send_message(message.chat.id, f"Hey! {message.from_user.first_name} - {message.from_user.id} \nWelcome To The new Era-Telegram")
+
+@bot.message_handler(commands=['tag'])
+def send_welcome(message):
+    bot.reply_to(message, "@727906232")
 
 
 bot.infinity_polling()
